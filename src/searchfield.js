18acd9ce67adcb54a3547d44e23483af38cfac4c
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2020-12-30 03:23:39 +0530

    commit

--- src/searchfield.js
+++ src/searchfield.js
@@ -4,13 +4,15 @@ function searchField(){
     return(
         <div className="bg-img justify-content-center">
             <form>
-                <div className="input-fields">
-                    <div className="input-container input-group d-flex justify-content-center">
-                            <input type="text" className="form-control form-control-lg col-sm-12 col-lg-6"
-                                placeholder="Search for a Recipe" />
-                        <button type="button" className="btn input-group-append search-btn btn-lg btn-grad">
-                            Search
-                        </button>
+                <div className="input-fields d-flex justify-content-center">
+                    <div className="col-sm-8 col-lg-6">
+                        <div className="input-container input-group">
+                                <input type="text" className="form-control form-control-md"
+                                    placeholder="Search for a Recipe" />
+                            <button type="button" className="btn input-group-append search-btn btn-md btn-grad">
+                                Search
+                            </button>
+                        </div>
                     </div>
                 </div>
             </form>
